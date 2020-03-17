db428e3c468d9e59b050b7c17c8c135201a2bd0e
anggi maulinda <anggi.maulinda@webimp.com.sg>
2020-03-17 16:21:31 +0700

    Updates

---
+++ precache-manifest.0d5e4ea1e50219ab848186359b5d0885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7af34a973c9afb02334285e6802dff",
+    "url": "/furniture-list/index.html"
+  },
+  {
+    "revision": "72e66681e605b698d105",
+    "url": "/furniture-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06160d10e443b2950974",
+    "url": "/furniture-list/static/js/2.612f191b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/furniture-list/static/js/2.612f191b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e66681e605b698d105",
+    "url": "/furniture-list/static/js/main.99eae2fa.chunk.js"
+  },
+  {
+    "revision": "7d742161e212cc306167",
+    "url": "/furniture-list/static/js/runtime-main.b894aee9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/furniture-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
